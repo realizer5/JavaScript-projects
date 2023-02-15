3cccb538cc66192882c7af04b4e3f1f9e99fde40
Realizer <107213639+realizer5@users.noreply.github.com>
2023-02-15 17:37:39 +0530

    Add files via upload

---
+++ tell me a joke/script.js
@@ -0,0 +1,298 @@
+let jokesArray = [` What do you call a boomerang that won’t come back?
+A stick.`,
+
+  `What does a cloud wear under his raincoat?
+Thunderwear.`,
+
+  `Two pickles fell out of a jar onto the floor. What did one say to the other?
+Dill with it.`,
+
+  `What time is it when the clock strikes 13?
+Time to get a new clock.`,
+
+  `How does a cucumber become a pickle?
+It goes through a jarring experience.`,
+
+  `What did one toilet say to the other?
+You look a bit flushed.`,
+
+  `What do you think of that new diner on the moon?
+Food was good, but there really wasn’t much atmosphere.`,
+
+  `Why did the dinosaur cross the road?
+Because the chicken wasn’t born yet.`,
+
+  `Why can’t Elsa from Frozen have a balloon?
+Because she will "let it go, let it go."`,
+
+  ` What musical instrument is found in the bathroom?
+A tuba toothpaste.`,
+
+  ` Why did the kid bring a ladder to school?
+Because she wanted to go to high school.`,
+
+  ` What do you call a dog magician?
+A labracadabrador.`,
+
+  ` Where would you find an elephant?
+The same place you lost her.`,
+
+  ` How do you get a squirrel to like you?
+Act like a nut.`,
+
+  ` What do you call two birds in love?
+Tweethearts`,
+
+  ` How does a scientist freshen her breath?
+With experi-mints.`,
+
+  ` How are false teeth like stars?
+They come out at night.`,
+
+  ` What building in your town has the most stories?
+The public library.`,
+
+  ` What’s worse than finding a worm in your apple?
+Finding half a worm.`,
+
+  ` What is a computer's favorite snack?
+Computer chips.`,
+
+  ` What did one volcano say to the other?
+I lava you.`,
+
+  ` How do we know that the ocean is friendly?
+It waves.`,
+
+  ` What is a tornado’s favorite game to play?
+Twister.`,
+
+  ` How does the moon cut his hair?
+Eclipse it.`,
+
+  ` How do you talk to a giant?
+Use big words.`,
+
+  ` What animal is always at a baseball game?
+A bat.`,
+
+  ` What falls in winter but never gets hurt?
+Snow.`,
+
+  ` What did the Dalmatian say after lunch?
+That hit the spot.`,
+
+  ` Why did the kid cross the playground?
+To get to the other slide.`,
+
+  ` What do you call a droid that takes the long way around?
+R2 detour.`,
+
+  ` Why did the cookie go to the hospital?
+Because he felt crummy.`,
+
+  ` Why was the baby strawberry crying?
+Because her mom and dad were in a jam.`,
+
+  ` What did the little corn say to the mama corn?
+Where is pop corn?`,
+
+  ` How do you make a lemon drop?
+Just let it fall.`,
+
+  ` What did the limestone say to the geologist?
+Don’t take me for granite.`,
+
+  ` Why does a seagull fly over the sea?
+Because if it flew over the bay, it would be a baygull.`,
+
+  ` What kind of water can’t freeze?
+Hot water.`,
+
+  ` What kind of tree fits in your hand?
+A palm tree.`,
+
+  ` What do you call a dinosaur that is sleeping?
+A dino-snore.`,
+
+  ` What is fast, loud and crunchy?
+A rocket chip.`,
+
+  ` Why did the teddy bear say no to dessert?
+Because she was stuffed.`,
+
+  ` What has ears but cannot hear?
+A cornfield.`,
+
+  ` What did the left eye say to the right eye?
+Between us, something smells.`,
+
+  ` What did one plate say to the other plate?
+Dinner is on me.`,
+
+  ` Why did the student eat his homework?
+Because the teacher told him it was a piece of cake.`,
+
+  ` When you look for something, why is it always in the last place you look?
+Because when you find it, you stop looking.`,
+
+  ` What is brown, hairy and wears sunglasses?
+A coconut on vacation.`,
+
+  ` What do you say to a rabbit on its birthday?
+Hoppy Birthday.`,
+
+  ` What’s the one thing will you get every year on your birthday, guaranteed?
+A year older.`,
+
+  ` Why do candles always go on the top of cakes?
+Because it's hard to light them from the bottom.`,
+
+  ` What do cakes and baseball teams have in common?
+They both need a good batter.`,
+
+  ` What goes up but never comes down?
+Your age.`,
+
+  ` What does every birthday end with?
+The letter Y.`,
+
+  ` What did the tiger say to her cub on his birthday?
+It's roar birthday.`,
+
+  ` Why did the girl put her cake in the freezer?
+She wanted to ice it.`,
+
+  ` Does a green candle burn longer than a pink one?
+No, they both burn shorter.`,
+
+  ` Why did the little girl hit her birthday cake with a hammer?
+It was a pound cake.`,
+
+  ` Yo Mama so small her best friend is an ant.
+`,
+  ` Yo Mama so old God signed her yearbook.
+`,
+  ` Yo Mama so short she has to hold a sign up that says, "Don't spit, I can't swim."
+`,
+  ` Yo Mama so small she has to slam-dunk her bus fare.
+`,
+  ` Yo Mama so old she rode dinosaurs to school.
+`,
+  ` Yo Mama so old her memory is in black and white.
+`,
+  ` Why didn't the quarter roll down the hill with the nickel?
+Because it had more cents.`,
+
+  ` Why is the obtuse triangle always so frustrated?
+Because it’s never right.`,
+
+  ` Why is six afraid of seven?
+Because seven eight nine.`,
+
+  ` Why was the equal sign so humble?
+Because he wasn’t greater than or less than anyone else.`,
+
+  ` What do you call guys who love math?
+Algebros.`,
+
+  ` How do you stay warm in any room?
+Go to the corner—it’s always 90 degrees.`,
+
+  ` Why was the fraction nervous about marrying the decimal?
+Because he would have to convert.`,
+
+  ` Are monsters good at math?
+Not unless you count Dracula.`,
+
+  ` Why was the math book sad?
+Because it had too many problems.`,
+
+  ` Why does nobody talk to circles?
+Because there’s no point.`,
+
+  ` Why couldn't the pony sing a lullaby?
+She was a little horse.`,
+
+  ` What was the first animal in space?
+The cow that jumped over the moon.`,
+
+  ` Why don’t elephants chew gum?
+They do, just not in public.`,
+
+  ` What did the banana say to the dog?
+Bananas can’t talk.`,
+
+  ` How do you make an octopus laugh?
+With ten-tickles.`,
+
+  ` What do you call a sleeping bull?
+A bull-dozer.`,
+
+  ` How do you fit more pigs on a farm?
+Build a sty-scraper.`,
+
+  ` What did the farmer call the cow that had no milk?
+An udder failure.`,
+
+  ` What do you call a cow that won't give milk?
+A milk dud.`,
+
+  ` Why do fish live in salt water?
+Because pepper makes them sneeze.`,
+
+  ` What do you get from a pampered cow?
+Spoiled milk.`,
+
+  ` Where do polar bears vote?
+The North Poll`,
+
+  ` What sound do porcupines make when they kiss?
+Ouch!`,
+
+  ` Why did the snake cross the road?
+To get to the other ssside.`,
+
+  ` Why are fish so smart?
+Because they live in schools.`,
+
+  ` What did the ocean say to the pirate?
+Nothing, it just waved.`,
+
+  ` Why don’t pirates shower before they walk the plank?
+Because they’ll just wash up on shore later.`,
+
+  ` What happened when Bluebeard fell overboard in the Red Sea?
+He got marooned.`,
+
+  ` How did the pirate get his flag so cheaply?
+He bought it on sail.`,
+
+  ` What has 8 legs, 8 arms, and 8 eyes?
+8 pirates.`,
+
+  ` How much does it cost a pirate to get his ears pierced?
+About a buck an ear.`,
+
+  ` Why is pirating so addictive?
+They say once ye lose yer first hand, ye get hooked.`,
+
+  ` How do pirates know that they are pirates?
+They think, therefore they arrr.`,
+
+  ` What is a cat's favorite color?
+Purrr-ple.`,
+
+  ` What song does a cat like best?
+Three Blind Mice.`,
+
+  ` Where did the school kittens go for their field trip?
+To the mew-seum.`,
+
+  `. What kind of kitten works for the Red Cross?
+A first-aid kit.`,];
+const tellJoke =() =>{
+let joke = jokesArray[Math.floor(Math.random()*(jokesArray.length-1))];
+let para =  document.getElementById("joke");
+  para.innerText = joke;
+}
